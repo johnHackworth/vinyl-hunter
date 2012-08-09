d9846977f14d5c767b3111c8a527e7ce7276f6c5
Javi Alvarez <jalvarez@youzee.com>
2012-08-09 11:09:20 +0200

    meta for table creation

--- artist/models/models.py
+++ artist/models/models.py
@@ -12,3 +12,7 @@ class Artist(ExtModel, models.Model):
     largeImage = models.CharField(max_length=255, default='')
 
     fields = ["id", "name", "lastFetched", "thumbnail", "image", "largeImage"]
+
+    class Meta:
+        app_label = 'artist'
+        db_table = 'artists'
--- lastFmUser/models/models.py
+++ lastFmUser/models/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from commons.models import ExtModel
-from datetime import datetime
 from artist.models.models import Artist
 
 
@@ -8,7 +7,10 @@ class LastFm_user(ExtModel, models.Model):
 
     name = models.CharField(max_length=255, default='')
     lastFetched = models.DateTimeField()
-    artists =  models.ManyToManyField(Artist)
-
+    artists = models.ManyToManyField(Artist)
 
     fields = ["id", "name", "lastFetched"]
+
+    class Meta:
+        app_label = 'lastFmUser'
+        db_table = 'lastFmUsers'
